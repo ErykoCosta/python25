dc47f5f20f39fb7a66a28466c590eb71f22e4a4e
Eryko <eryko4350@gmail.com>
2025-03-08 17:32:42 -0300

    melhorando com "if not"

--- python25.py
+++ python25.py
@@ -21,8 +21,28 @@ opcao = input(texto + '\nDigite sua escolha (1/2): ')
 while opcao not in ['1', '2']:
     print('Opção inválida! Tente novamente.')
     opcao = input(texto + '\nDigite sua escolha (1/2): ')
-    
+
+mineral = 2.50
+gas = 3.00 
+qtdade = ''
+valor_total = 0
+
 if opcao == '1':
-    print('Você escolheu água mineral. Sua conta deu R$ 2,50.')
+    print('Você escolheu água mineral. O valor é R$', mineral)
+    while not qtdade.isdigit():
+        qtdade = input('Quantas garrafas deseja comprar? ')
+        if not qtdade.isdigit():
+            print('Digite um número inteiro.')
+    qtdade = int(qtdade)
+    valor_total = qtdade * mineral
+    print('O valor total da sua compra é R$', valor_total)
+    
 elif opcao == '2':
-    print('Você escolheu água com gás. Sua conta deu R$ 3,00.')
+    print('Você escolheu água com gás. O valor é R$', gas)
+    while not qtdade.isdigit():
+        qtdade = input('Quantas garrafas deseja comprar? ')
+        if not qtdade.isdigit():
+            print('Digite um número inteiro.')
+    qtdade = int(qtdade)
+    valor_total = qtdade * gas
+    print('O valor total da sua compra é R$', valor_total)
